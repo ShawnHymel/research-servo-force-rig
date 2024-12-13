dcf67130aa3cac12bb3f5334e67c5b7aaa7b383f
ShawnHymel <5232145+ShawnHymel@users.noreply.github.com>
2024-12-13 08:55:28 -0700

    Redid data collection for SG92R

--- software/python/move_servo.py
+++ software/python/move_servo.py
@@ -5,7 +5,9 @@
 import serial
 import sys
 
-from servo_sensor_logging import run_test
+from servo_sensor_logging import run_test, read_serial_data
+
+INITIAL_TIMEOUT = 10.0
 
 # Read first argmument from command line as servo position
 if len(sys.argv) < 2:
@@ -19,12 +21,18 @@ BAUD_RATE = 912600
 TIMEOUT = 1.0
 
 # Construct message
-msg = f"{servo_pos}, 0, {servo_pos}, 0, 0"
+msg = f"{servo_pos}, 0, {servo_pos}, 30, 1"
 
 # Send message to Arduino
 try:
     with serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=1) as ser:
         ser.write(msg.encode())
+
+        # Wait for the response
+        resp = read_serial_data(ser, INITIAL_TIMEOUT, TIMEOUT)
+        for line in resp:
+            print(line)
+
 except serial.SerialException as e:
     print(f"Error opening or using the serial port: {e}")
 except Exception as e:
